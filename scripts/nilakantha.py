9e806963ddc9865f11ac46e29c10fab3439ce718
Ines de Santiago <inesdesantiago@gmail.com>
2014-07-13 17:36:16 +0100

    added nilakantha exercise solution

--- scripts/nilakantha.py
+++ scripts/nilakantha.py
@@ -1,3 +1,11 @@
+#Possible advanced exercise for end of section 2
+#### Text to past in the notebook:
+'''
+__Advanced exercise__
+Determine how many iterations it takes to calculate $\pi$ to 8 decimal places using the Nilakintha series:
+$$3+\sum_{n=1}\frac{4(-1)^{n+1}}{2n(2n+1)(2n+2)}$$
+'''
+
 def nilakantha(end): 
     op = '+'
     x = 3.0
