889c5a1bc2b8efe335c82e2f9886a65f2884c6ee
Ines de Santiago <inesdesantiago@gmail.com>
2014-07-13 18:15:08 +0100

    Solution to exercise 1_0.py

---
+++ solutions/1_0.py
@@ -0,0 +1,20 @@
+#Exercise should be done in the interpreter
+
+name = "myname"
+age = 21
+
+print name, "is", age
+
+
+
+
+age += 10
+
+print name, "will be", str(age), "in 10 years"
+
+
+
+v1 = 5.0
+v2 = 10.0
+mean = (v1 + v2) / 2
+print mean
