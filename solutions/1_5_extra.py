bacee81dee000e1a2af1b7e63ac7af5b428fbb73
pajanne <pajanne@gmail.com>
2015-04-30 15:13:46 +0100

    solution to advanced exercice 1_5_extra on dictionary without loop

--- solutions/1_5_extra.py
+++ solutions/1_5_extra.py
@@ -1,19 +1,41 @@
-lysSeq = "MKALIVLGLVLLSVTVQGKVFERCELARTLKRLGMDGYRGISLANWMCLAKWESGYNTRATNYNAGDRSTDYGIFQINSRYWCNDGKTPGAVNACHLSCSALLQDNIADAVACAKRVVRDPQGIRAWVAWRNRCQNRDVRQYVQGCGV"
+seq = "MKALIVLGLVLLSVTVQGKVFERCELARTLKRLGMDGYRGISLANWMCLAKWESGYNTRATNYNAGDRSTDYGIFQINSRYWCNDGKTPGAVNACHLSCSALLQDNIADAVACAKRVVRDPQGIRAWVAWRNRCQNRDVRQYVQGCGV"
 
-#set(lysSeq)
-#set(['A', 'C', 'E', 'D', 'G', 'F', 'I', 'H', 'K', 'M', 'L', 'N', 'Q', 'P', 'S', 'R', 'T', 'W', 'V', 'Y']
+# unique amino acids in the sequence
+unique_aa = set(list(seq))
+
+# sorted amino acids
+unique_aa_sorted = sorted(list(unique_aa))
+print unique_aa_sorted
+
+# storing the occurences in a dictionary
+aa_counts = {}
+aa_counts['A'] = seq.count('A')
+aa_counts['C'] = seq.count('C')
+aa_counts['D'] = seq.count('D')
+# etc...
+
+print aa_counts
+
+# printing the results
+print 'A has', aa_counts['A'], 'occurrence(s)'
+print 'C has', aa_counts['C'], 'occurrence(s)'
+print 'D has', aa_counts['D'], 'occurrence(s)'
+print "etc..."
 
 
-#Add counts one by one to an empty dictionary
-aaCounts = {}
-for aa in set(lysSeq):
-	aaCounts[aa] = lysSeq.count(aa)
+# To fully solve this problem, you'll need to learn about loops which are described
+# in the next course section
 
-#Sort the keys of the dictionary in alphabetic order:
-keys = aaCounts.keys()
+# Add counts one by one to an empty dictionary
+aa_counts = {}
+for aa in set(seq):
+    aa_counts[aa] = seq.count(aa)
+
+# Sort the keys of the dictionary in alphabetic order:
+keys = aa_counts.keys()
 keys.sort()
 
-#print the results in the screen by alphabetic key order:
+# Print the results by key sorted by alphabetic order
 for aa in keys:
-	print "%s: %i" %(aa, aaCounts[aa])
+    print "%s has %i occurrence(s)" % (aa, aa_counts[aa])
 
