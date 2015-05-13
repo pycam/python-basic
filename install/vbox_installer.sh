7a3d3844fb9f8deb01fad372b0c8ff1e2650854e
pajanne <pajanne@gmail.com>
2015-05-13 16:39:39 +0100

    add fasta file containing sequences for running the blast commands; update virtualbox installer for lubuntu LTS 14.4

--- install/vbox_installer.sh
+++ install/vbox_installer.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
-# PyCam ubuntu installer
+# PyCam lubuntu LTS 14.04 VirtualBox installer based on lubuntu-14.04.2-desktop-i386
 
 sudo su -
 apt-get install gedit
+apt-get install vim
 apt-get install git
 apt-get install python-pip
 apt-get install python-zmq
 apt-get install python-matplotlib
 apt-get install python-biopython
+apt-get install ncbi-blast+
 
-apt-get install virtualbox-guest-utils
-apt-get install dkms
+# Install VirtualBox Additions
+# From the VirtualBox menu of lubuntu go to Devices > Insert Guest Additions CD image... and do
+cd /media/training/VBOXADDITIONS_4.3.26_98988
+sudo ./VBoxLinuxAdditions.run
 
 pip install ipython[notebook]
 
@@ -19,3 +23,7 @@ apt-get clean
 exit
 
 git clone https://github.com/pycam/python-intro.git course
+
+# Add ipython at startup from lubuntu menu do to...
+# Preferences > Default applications for LXSession then tab Autostart and add:
+# /usr/local/bin/ipython notebook --no-browser --port=8888 --ip=127.0.0.1 /home/training/course/
